0f8d335b4525be67241e40fa2da7198945e05e27
Sami Hindi <hindis@bzz.ch>
2022-12-08 09:22:37 +0100

    Structure cleaning

---
+++ src/PyHostr/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='PyHostr',
+    author='Sami Hindi',
+    author_email='sami@samihindi.com',
+    version='0.5',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    description='Python module for hosting your files, with routing.',
+    url='https://github.com/FujiwaraChoki/PyHostr'
+)
